38a1d165476365ac42361b849c9ba750792cb35f
WhaleVsPlastic <gustavomartinsesilva3@gmail.com>
2024-12-31 18:45:56 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,137 @@
+const translations = {
+    en: {
+        greeting: "My Whale Game",
+        download: "Download",
+        downloadMac: "Download for Mac",
+        downloadWindows: "Download for Windows",
+        descriptionTitle: "Description",
+        descriptionText: "Embark on an underwater adventure with a brave whale determined to protect its kind. Navigate the vast ocean, facing off against threats that have endangered the whale's species. Because this is the first version of the game there are no 'boss' battles or upgrades and the game will only be available in English. Yet, you'll encounter three unique enemies to challenge your skills",
+        gameplayImagesTitle: "Gameplay Images",
+    },
+    fr: {
+        greeting: "Mon Jeu de Baleine",
+        download: "Télécharger",
+        downloadMac: "Télécharger pour Mac",
+        downloadWindows: "Télécharger pour Windows",
+        descriptionTitle: "Description",
+        descriptionText: "Embarquez pour une aventure sous-marine avec une brave baleine déterminée à protéger sa race. Naviguez à travers l'océan vaste, affrontant les menaces qui ont mis en danger l'espèce de la baleine. Comme il s'agit de la première version du jeu, il n'y a pas encore de combats contre des 'boss' ni de mises à niveau, et le jeu ne sera disponible qu'en anglais. Vous rencontrerez trois ennemis uniques pour tester vos compétences",
+        gameplayImagesTitle: "Images de Jeu",
+    },
+    pt: {
+        greeting: "Meu Jogo de Baleia",
+        download: "Baixar",
+        downloadMac: "Baixar para Mac",
+        downloadWindows: "Baixar para Windows",
+        descriptionTitle: "Descrição",
+        descriptionText: "Embarque em uma aventura subaquática com uma baleia corajosa determinada a proteger sua espécie. Navegue pelo vasto oceano, enfrentando ameaças que colocaram a espécie da baleia em perigo. Como esta é a primeira versão do jogo, não há batalhas de 'chefes' ou melhorias ainda, e o jogo estará disponível apenas em inglês. Você encontrará três inimigos únicos para desafiar suas habilidades",
+        gameplayImagesTitle: "Imagens de Jogo",
+    },
+    de: {
+        greeting: "Mein Walspiel",
+        download: "Herunterladen",
+        downloadMac: "Für Mac herunterladen",
+        downloadWindows: "Für Windows herunterladen",
+        descriptionTitle: "Beschreibung",
+        descriptionText: "Begib dich auf ein Unterwasserabenteuer mit einem mutigen Wal, der entschlossen ist, seine Art zu schützen. Navigiere durch den weiten Ozean und stelle dich Bedrohungen, die die Walart gefährden. Da dies die erste Version des Spiels ist, gibt es noch keine 'Boss'-Kämpfe oder Upgrades, und das Spiel wird nur in Englisch verfügbar sein. Du wirst drei einzigartige Feinde treffen, die deine Fähigkeiten herausfordern",
+        gameplayImagesTitle: "Gameplay Bilder",
+    },
+    es: {
+        greeting: "Mi Juego de Ballena",
+        download: "Descargar",
+        downloadMac: "Descargar para Mac",
+        downloadWindows: "Descargar para Windows",
+        descriptionTitle: "Descripción",
+        descriptionText: "¡Emprende una aventura submarina con una valiente ballena decidida a proteger a su especie. Navega por el vasto océano enfrentándote a amenazas que han puesto en peligro a la especie de la ballena. Debido a que esta es la primera versión del juego, aún no hay batallas contra 'jefes' ni mejoras, y el juego solo estará disponible en inglés. Te encontrarás con tres enemigos únicos que desafiarán tus habilidades",
+        gameplayImagesTitle: "Imágenes del Juego",
+    },
+    ru: {
+        greeting: "Моя Игра Кита",
+        download: "Скачать",
+        downloadMac: "Скачать для Mac",
+        downloadWindows: "Скачать для Windows",
+        descriptionTitle: "Описание",
+        descriptionText: "Отправьтесь в подводное приключение с храбрым китом, решившим защитить свой вид. Плавать по огромному океану, сражаясь с угрозами, которые ставят под угрозу вид кита. Поскольку это первая версия игры, в ней нет боев с 'боссами' или улучшений, и игра будет доступна только на английском. Вы встретите троих уникальных врагов, чтобы проверить свои навыки",
+        gameplayImagesTitle: "Изображения Игры",
+    },
+    it: {
+        greeting: "Il Mio Gioco della Balena",
+        download: "Scaricare",
+        downloadMac: "Scarica per Mac",
+        downloadWindows: "Scarica per Windows",
+        descriptionTitle: "Descrizione",
+        descriptionText: "Inizia un'avventura sottomarina con una balena coraggiosa determinata a proteggere la sua specie. Naviga nell'oceano vasto affrontando le minacce che hanno messo in pericolo la specie della balena. Poiché questa è la prima versione del gioco, non ci sono ancora battaglie contro i 'boss' o miglioramenti, e il gioco sarà disponibile solo in inglese. Incontrerai tre nemici unici che metteranno alla prova le tue abilità",
+        gameplayImagesTitle: "Immagini di Gioco",
+    },
+    nl: {
+        greeting: "Mijn Walvis Spel",
+        download: "Downloaden",
+        downloadMac: "Download voor Mac",
+        downloadWindows: "Download voor Windows",
+        descriptionTitle: "Beschrijving",
+        descriptionText: "Ga op een onderwateravontuur met een dappere walvis die vastbesloten is zijn soort te beschermen. Navigeer door de uitgestrekte oceaan en ga de strijd aan met bedreigingen die de walvissoort in gevaar hebben gebracht. Omdat dit de eerste versie van het spel is, zijn er nog geen 'baas'-gevechten of upgrades. Het spel zal alleen beschikbaar zijn in het Engels. Je zult drie unieke vijanden tegenkomen om je vaardigheden op de proef te stellen",
+        gameplayImagesTitle: "Gameplay Afbeeldingen",
+    },
+    tr: {
+        greeting: "Benim Balina Oyunum",
+        download: "İndir",
+        downloadMac: "Mac için indir",
+        downloadWindows: "Windows için indir",
+        descriptionTitle: "Açıklama",
+        descriptionText: "Kendi türünü korumaya kararlı cesur bir balina ile su altı macerasına atıl. Geniş okyanusu keşfederek, balina türünü tehlikeye atan tehditlerle yüzleş. Bu oyunun ilk versiyonu olduğundan, henüz 'boss' savaşları veya yükseltmeler yok, oyun sadece İngilizce olarak sunulacak. Üç benzersiz düşmanla becerilerini sınayacaksın",
+        gameplayImagesTitle: "Oyun Görüntüleri",
+    },
+    sv: {
+        greeting: "Mitt Valspel",
+        download: "Ladda ner",
+        downloadMac: "Ladda ner för Mac",
+        downloadWindows: "Ladda ner för Windows",
+        descriptionTitle: "Beskrivning",
+        descriptionText: "Ge dig ut på ett undervattensäventyr med en modig val som är fast besluten att skydda sin art. Navigera genom det stora havet och möt hot som har satt valens art i fara. Eftersom detta är den första versionen av spelet finns det inga 'boss'-strider eller uppgraderingar än, och spelet kommer endast vara tillgängligt på engelska. Du kommer att stöta på tre unika fiender som utmanar dina färdigheter",
+        gameplayImagesTitle: "Spelbilder",
+    },
+    no: {
+        greeting: "Mitt Hvalspill",
+        download: "Last ned",
+        downloadMac: "Last ned for Mac",
+        downloadWindows: "Last ned for Windows",
+        descriptionTitle: "Beskrivelse",
+        descriptionText: "Bli med på et undervanns-eventyr med en modig hval som er fast bestemt på å beskytte sin art. Naviger gjennom det enorme havet og stå overfor trusler som har satt hvalens art i fare. Siden dette er den første versjonen av spillet, finnes det ingen 'bosser' eller oppgraderinger ennå, og spillet vil kun være tilgjengelig på engelsk. Du vil møte tre unike fiender for å utfordre ferdighetene dine",
+        gameplayImagesTitle: "Gameplay Bilder",
+    },
+    da: {
+        greeting: "Mit Hvalspil",
+        download: "Download",
+        downloadMac: "Download til Mac",
+        downloadWindows: "Download til Windows",
+        descriptionTitle: "Beskrivelse",
+        descriptionText: "Tag på et undervands eventyr med en modig hval, der er fast besluttet på at beskytte sin art. Naviger gennem det enorme ocean og mød trusler, der har sat hvalens art i fare. Da dette er den første version af spillet, er der endnu ingen 'bosser' eller opgraderinger, og spillet vil kun være tilgængeligt på engelsk. Du vil møde tre unikke fjender, der udfordrer dine færdigheder",
+        gameplayImagesTitle: "Gameplay Billeder",
+    },
+    fi: {
+        greeting: "Valas Pelini",
+        download: "Lataa",
+        downloadMac: "Lataa Macille",
+        downloadWindows: "Lataa Windowsille",
+        descriptionTitle: "Kuvaus",
+        descriptionText: "Lähde vedenalaiselle seikkailulle urhealla valalla, joka on päättänyt suojella omaa lajiaan. Navigoi valtavassa meressä kohdataksesi uhkia, jotka ovat vaarantaneet valaan lajin. Koska tämä on pelin ensimmäinen versio, ei ole vielä 'pomo' -taisteluita tai parannuksia, ja peli on saatavilla vain englanniksi. Törmäät kolmeen ainutlaatuiseen viholliseen, jotka haastavat taitosi",
+        gameplayImagesTitle: "Pelin Kuvia",
+    },
+};
+
+const languageSelect = document.querySelector('.language');
+
+function updateContent(language) {
+    document.getElementById('game-title').innerText = translations[language].greeting;
+    document.getElementById('download-title').innerText = translations[language].download;
+    document.getElementById('mac-btn-text').innerHTML = translations[language].downloadMac + ' <i class="fa-brands fa-apple"></i>';
+    document.getElementById('win-btn-text').innerHTML = translations[language].downloadWindows + ' <i class="fa-brands fa-windows"></i>';
+    document.getElementById('description-title').innerText = translations[language].descriptionTitle;
+    document.getElementById('description-text').innerHTML = translations[language].descriptionText.split('. ').join('<br><br>');
+    document.getElementById('gameplay-images-title').innerText = translations[language].gameplayImagesTitle;
+}
+
+languageSelect.addEventListener('change', (event) => {
+    updateContent(event.target.value);
+});
+
+updateContent('en');
